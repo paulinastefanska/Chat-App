ca27b31d0932f9bd5acfff874fbeb0cd3b762816
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-09-02 11:48:58 +0200

    Import users service

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const socketIo = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+const UsersService = require('./UsersService');
+
+const usersService = new UsersService();
 
 app.use(express.static(`${__dirname}/public`));
 
