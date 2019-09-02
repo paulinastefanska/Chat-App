48c3d64ac36540be2756b85b611337b63f132c0f
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-09-02 13:43:02 +0200

    Add client App.js

---
+++ client/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+import io from 'socket.io-client';
+
+const socket = io('/');
+
+class App extends Component {
+	constructor(props) {
+    super(props);
+    this.state = {users: [], messages: [], text: '', name: ''};
+  }
+};
+
+export default hot(module)(App);
+
+render() {
+    return this.state.name !== '' ? this.renderLayout() : this.renderUserForm();
+}
+
+renderLayout() {
+   return (
+      <div className={styles.App}>
+        <div className={styles.AppHeader}>
+          <div className={styles.AppTitle}>
+            ChatApp
+          </div>
+          <div className={styles.AppRoom}>
+            App room
+          </div>
+        </div>
+        <div className={styles.AppBody}>
+          <UsersList
+            users={this.state.users}
+          />
+          <div className={styles.MessageWrapper}>
+            <MessageList
+              messages={this.state.messages}
+            />
+            <MessageForm
+              onMessageSubmit={message => this.handleMessageSubmit(message)}
+              name={this.state.name}
+            />
+          </div>
+        </div>
+      </div>
+   );
+}
