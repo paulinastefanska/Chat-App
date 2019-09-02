c792ed1acf98824e5f66163afbde91f2945cc5f3
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-09-02 11:29:44 +0200

    Add new chat user

--- index.js
+++ index.js
@@ -12,6 +12,21 @@ app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
+io.on('connection', (socket) => {
+	// klient nasłuchuje na wiadomość wejścia do czatu
+	socket.on('join', (name) => {
+	  // użytkownika, który pojawił się w aplikacji, zapisujemy do serwisu trzymającego listę osób w czacie
+	  usersService.addUser({
+	    id: socket.id,
+	    name
+	  });
+	  // aplikacja emituje zdarzenie update, które aktualizuje informację na temat listy użytkowników każdemu nasłuchującemu na wydarzenie 'update'
+	  io.emit('update', {
+	    users: usersService.getAllUsers()
+	  });
+	});
+});
+
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
